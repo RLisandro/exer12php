61e38f0756eb1297faa1999f446f826317bb8eae
Lisandro Rezende Cazuza <rezendecazuza@gmail.com>
2024-05-22 20:41:57 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercico 12</title>
+</head>
+<body>
+ <!--Escreva um algoritmo que receba um valor, 
+ calcule e mostre um desconto de 27%.    -->   
+ <label >
+    <h1>Desconto de 27% </h1>
+    <form  method="post">
+        Valor: <input type="text" name="valor" ><br><br>
+        <input type="submit" value="Calcular">
+    </form>
+ </label>
+</body>
+<?php 
+
+
+
+?>
+</html>
