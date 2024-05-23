ba3e28067277ca228ca7b711ce4ba4a97c04554d
Lisandro Rezende Cazuza <rezendecazuza@gmail.com>
2024-05-22 21:59:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -12,13 +12,13 @@
     <h1>Desconto de 27% </h1>
     <form  method="post">
         Valor: <input type="text" name="valor" ><br><br>
-        <input type="submit" value="Calcular">
+        <input type="submit" value="Calcular"><br><br>
     </form>
  </label>
 </body>
 <?php 
-
-
-
+$Valor =$_POST['valor'];
+$desconto = ($Valor* 27) /100;
+echo"O desconto de 27%  de R$ " .$Valor. ". <br>São R$ " .$desconto;
 ?>
 </html>
